a6868d0f971daa05dac5e105e68af36e5261672b
Ilya Titkov <ilya@titkov.me>
2022-09-16 21:40:29 +0300

    things

--- setup-users.sh
+++ setup-users.sh
@@ -2,18 +2,10 @@
 
 sudo useradd sonarr -u 13001
 sudo useradd radarr -u 13002
-sudo useradd lidarr -u 13003
-sudo useradd readarr -u 13004
-sudo useradd mylar -u 13005
 sudo useradd prowlarr -u 13006
 sudo useradd qbittorrent -u 13007
-sudo useradd jackett -u 13008
 sudo groupadd mediacenter -g 13000
 sudo usermod -a -G mediacenter sonarr
 sudo usermod -a -G mediacenter radarr
-sudo usermod -a -G mediacenter lidarr
-sudo usermod -a -G mediacenter readarr
-sudo usermod -a -G mediacenter mylar
 sudo usermod -a -G mediacenter prowlarr
 sudo usermod -a -G mediacenter qbittorrent
-sudo usermod -a -G mediacenter jackett
--- setup.sh
+++ setup.sh
@@ -2,17 +2,14 @@
 
 
 # Make directories
-sudo mkdir -pv ${ROOT_DIR}/docker/{sonarr,radarr,lidarr,readarr,mylar,prowlarr,qbittorrent,jackett}-config
-sudo mkdir -pv ${ROOT_DATA_DIR}/data/{torrents,media}/{tv,movies,music,books,comics}
+sudo mkdir -pv ${ROOT_DIR}/docker/{sonarr,radarr,prowlarr,qbittorrent}-config
+sudo mkdir -pv ${ROOT_DATA_DIR}/data/{torrents,media}/{tv,movies}
 
 # Set permissions
 sudo chmod -R 775 ${ROOT_DATA_DIR}/data/
 sudo chown -R $(id -u):mediacenter ${ROOT_DATA_DIR}/data/
 sudo chown -R sonarr:mediacenter ${ROOT_DIR}/docker/sonarr-config
 sudo chown -R radarr:mediacenter ${ROOT_DIR}/docker/radarr-config
-sudo chown -R lidarr:mediacenter ${ROOT_DIR}/docker/lidarr-config
-sudo chown -R readarr:mediacenter ${ROOT_DIR}/docker/readarr-config
-sudo chown -R mylar:mediacenter ${ROOT_DIR}/docker/mylar-config
 sudo chown -R prowlarr:mediacenter ${ROOT_DIR}/docker/prowlarr-config
 sudo chown -R qbittorrent:mediacenter ${ROOT_DIR}/docker/qbittorrent-config
 sudo chown -R jackett:mediacenter ${ROOT_DIR}/docker/jackett-config
